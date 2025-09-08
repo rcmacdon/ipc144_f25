9fe84ce7de4f9b6e1b9f3f18f46b47abd8042d00
Clint <clint.macdonald@durhamcollege.ca>
2025-09-08 08:28:14 -0400

    added stddef.h file for download and include. in mingw include folder.

---
+++ Week1/HelloWorld.c
@@ -0,0 +1,15 @@
+// ------------------------------
+// File:        HelloWorld.c
+// Author:      Clint MacDonald
+// Date:        Sept. 2, 2025
+// Purpose:     My First Program
+// ------------------------------
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World!\n");
+    char myName[] = "Clint MacDonald";
+    printf("My name is %s\n", myName);
+    int myNum = 16;
+    printf("My favorite number is %i\n", myNum);
+}
